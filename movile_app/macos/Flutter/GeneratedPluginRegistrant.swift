8df31b23df2dabf39e50f5d320a9228706ca2c98
PabloMartinezIbanez <pablomartinezibanez04@gmail.com>
2026-07-05 13:09:35 +0200

    Refactor and remove Windows runner files for Flutter application
    - Deleted FlutterWindow class and its associated files (flutter_window.cpp, flutter_window.h, main.cpp, resource.h, runner.exe.manifest, utils.cpp, utils.h, win32_window.cpp, win32_window.h) to streamline the Windows runner implementation.
    - Added new CMakeLists.txt for Linux and Windows platforms to manage build configurations.
    - Introduced generated_plugin_registrant.cc and generated_plugin_registrant.h for plugin registration on both Linux and Windows.
    - Updated generated_plugins.cmake to include necessary plugins for both platforms.
    - Created Flutter-Debug.xcconfig and Flutter-Release.xcconfig for macOS configurations.
    - Added GeneratedPluginRegistrant.swift for macOS plugin registration.

---
+++ movile_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,38 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import app_links
+import audioplayers_darwin
+import connectivity_plus
+import device_info_plus
+import file_selector_macos
+import flutter_image_compress_macos
+import geolocator_apple
+import google_sign_in_ios
+import package_info_plus
+import share_plus
+import shared_preferences_foundation
+import sqflite_darwin
+import url_launcher_macos
+import wakelock_plus
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
+}
